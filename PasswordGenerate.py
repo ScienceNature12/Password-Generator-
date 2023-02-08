fcc79814583eaaaa756da8d953b8916d37260af2
ScienceNature <47803208+ScienceNature12@users.noreply.github.com>
2023-02-07 23:47:07 -0800

    Update PasswordGenerate.py
    Made the password randomly generate a password of specified length.

--- PasswordGenerate.py
+++ PasswordGenerate.py
@@ -1,29 +1,16 @@
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-           'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
+import secrets
+import string
 
-print("Welcome to the password generator!")
-random_letters = int(input(f"How many letters do you want?\n"))
-random_symbols = int(input(f"How many symbols do you want?\n"))
-random_numbers = int(input(f"How many numbers do you want?\n"))
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+password = letters + numbers + symbols
+pwd_length = 11
 
-passwords = []
+pwd = ""
 
-for char in range(1, random_letters + 1):
-    passwords.append(random.choice(letters))
+for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(password))
 
-for char in range(1, random_symbols + 1):
-    passwords.append(random.choice(symbols))
-
-for char in range(1, random_numbers + 1):
-    passwords.append(random.choice(numbers))
-
-random.shuffle(passwords)
-password = ""
-for char in passwords:
-    password += char
-print(f"Your Password is: {password}")
+print(pwd)
 
